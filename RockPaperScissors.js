dff4ad7211831a4eb6dd92d17f9bb7e72b04dc3f
saltysam14 <92763644+saltysam14@users.noreply.github.com>
2021-11-21 20:48:27 -0500

    Added UI for RockPaperScissors.html and RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,59 +1,36 @@
 let arrayOptions = ["Rock", "Paper", "Scissors"];
-var playerSelection = window.prompt("Rock, Paper or Scissors?: ");
 var computerSelection=computerPlay();
-
+var computerScore=0;
+var playerScore=0;
+let buttons=document.querySelectorAll(".btn");
+var computerSelection=computerPlay().toLowerCase();
+let playerScoreDisplay = document.querySelector('#playerScore');
+let computerScoreDisplay = document.querySelector('#computerScore');
+let playerSelectionDisplay = document.querySelector('#playerSelection');
+let computerSelectionDisplay = document.querySelector('#computerSelection');
+let finalResult = document.querySelector('#finalResult');
 
 function computerPlay() {
     return arrayOptions[Math.floor(Math.random()*arrayOptions.length)];
 }
+buttons.forEach((button) => {
+    button.addEventListener("click", getPlayerChoice);
+}) 
 
-function playRound (playerSelection, computerSelection) {
-
+function getPlayerChoice(e) {
+    playerSelection = (e.target.getAttribute('id'));
     
-
-    var computerSelection=computerPlay().toLowerCase();
-    var playerSelection=playerSelection.toLowerCase();
-      
-    
-    
-    if  (computerSelection == playerSelection)  {
-        alert("Tie Game!");
-     }
-     else if (
-        computerSelection == "rock" && playerSelection == "paper") {
-            alert("You Win! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "rock" && playerSelection == "scissors") {
-            alert("You Lose! Rock beats Scissors");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "rock") {
-            alert("You Lose! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "scissors") {
-            alert("You Win! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "paper") {
-            alert("You Lose! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "rock") {
-            alert("You Win! Rock beats Scissors");
-        }
-    
+    game(playerSelection, computerSelection);
+   
 }
 
 function game() {
 
-    var computerScore=0;
-    var playerScore=0;
-    var playerSelection = window.prompt("Rock, Paper or Scissors?: ");
+    
+    getPlayerChoice;
     var computerSelection=computerPlay().toLowerCase();
-    var playerSelection=playerSelection.toLowerCase();
-    if  (computerSelection == playerSelection)  {
+    
+    if  (computerSelection === playerSelection)  {
         alert("Tie Game!");
      }
      else if (
@@ -86,229 +63,32 @@ function game() {
             (computerSelection == "rock" && playerSelection == "scissors") ||
             (computerSelection == "paper" && playerSelection == "rock") ||
             (computerSelection == "scissors" && playerSelection == "paper"))
-            ++computerScore;
+            computerScoreDisplay.textContent="Computer Score:" + " " + ++computerScore;
             
     
-            else if(
+        else if(
             (computerSelection == "rock" && playerSelection == "paper") ||
             (computerSelection == "paper" && playerSelection == "scissors") ||
             (computerSelection == "scissors" && playerSelection == "rock")) 
-        ++playerScore;
+            playerScoreDisplay.textContent="Player Score:" + " " + ++playerScore;
 
 
+    playerSelectionDisplay.textContent="Player Choice:" + " " + playerSelection;
+    computerSelectionDisplay.textContent="Computer Choice:" + " " + computerSelection;
+   
     
-    
-    console.log(playerSelection, computerSelection);
-    console.log(playerScore, computerScore);
-    
-
-        var playerSelection = window.prompt("Rock, Paper or Scissors?: ");
-        var computerSelection=computerPlay().toLowerCase();
-        var playerSelection=playerSelection.toLowerCase();
-        if  (computerSelection == playerSelection)  {
-        alert("Tie Game!");
-     }
-     else if (
-        computerSelection == "rock" && playerSelection == "paper") {
-            alert("You Win! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "rock" && playerSelection == "scissors") {
-            alert("You Lose! Rock beats Scissors");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "rock") {
-            alert("You Lose! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "scissors") {
-            alert("You Win! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "paper") {
-            alert("You Lose! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "rock") {
-            alert("You Win! Rock beats Scissors");
-        }
-              if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "paper" && playerSelection == "rock") ||
-        (computerSelection == "scissors" && playerSelection == "paper"))
-        ++computerScore;
         
+    if (computerScore === 5) {
+        finalResult.textContent="You lose! Game over.";
+        buttons.forEach((button) => {
+        button.removeEventListener("click", getPlayerChoice);
+    });
+}
 
-        else if(
-        (computerSelection == "rock" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "rock")) 
-    ++playerScore;
-
-
-    console.log(playerSelection, computerSelection);
-    console.log(playerScore, computerScore);
-
-
-        var playerSelection = window.prompt("Rock, Paper or Scissors?: ");
-        var computerSelection=computerPlay().toLowerCase();
-        var playerSelection=playerSelection.toLowerCase();
-        if  (computerSelection == playerSelection)  {
-        alert("Tie Game!");
-     }
-     else if (
-        computerSelection == "rock" && playerSelection == "paper") {
-            alert("You Win! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "rock" && playerSelection == "scissors") {
-            alert("You Lose! Rock beats Scissors");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "rock") {
-            alert("You Lose! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "scissors") {
-            alert("You Win! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "paper") {
-            alert("You Lose! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "rock") {
-            alert("You Win! Rock beats Scissors");
-        }
-
-          if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "paper" && playerSelection == "rock") ||
-        (computerSelection == "scissors" && playerSelection == "paper"))
-        ++computerScore;
-        
-
-        else if(
-        (computerSelection == "rock" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "rock")) 
-    ++playerScore;
-
-
-    console.log(playerSelection, computerSelection);
-    console.log(playerScore, computerScore);
-
-
-
-        var playerSelection = window.prompt("Rock, Paper or Scissors?: ");
-        var computerSelection=computerPlay().toLowerCase();
-        var playerSelection=playerSelection.toLowerCase();
-        if  (computerSelection == playerSelection)  {
-        alert("Tie Game!");
-     }
-     else if (
-        computerSelection == "rock" && playerSelection == "paper") {
-            alert("You Win! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "rock" && playerSelection == "scissors") {
-            alert("You Lose! Rock beats Scissors");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "rock") {
-            alert("You Lose! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "scissors") {
-            alert("You Win! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "paper") {
-            alert("You Lose! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "rock") {
-            alert("You Win! Rock beats Scissors");
-        }
-          if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "paper" && playerSelection == "rock") ||
-        (computerSelection == "scissors" && playerSelection == "paper"))
-        ++computerScore;
-        
-
-        else if(
-        (computerSelection == "rock" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "rock")) 
-    ++playerScore;
-
-    console.log(playerSelection, computerSelection);
-    console.log(playerScore, computerScore);
-
-
-
-        var playerSelection = window.prompt("Rock, Paper or Scissors?: ");
-        var computerSelection=computerPlay().toLowerCase();
-        var playerSelection=playerSelection.toLowerCase();
-        if  (computerSelection == playerSelection)  {
-        alert("Tie Game!");
-     }
-     else if (
-        computerSelection == "rock" && playerSelection == "paper") {
-            alert("You Win! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "rock" && playerSelection == "scissors") {
-            alert("You Lose! Rock beats Scissors");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "rock") {
-            alert("You Lose! Paper beats Rock");
-        }
-    else if (
-        computerSelection == "paper" && playerSelection == "scissors") {
-            alert("You Win! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "paper") {
-            alert("You Lose! Scissors beats Paper");
-        }
-    else if (
-        computerSelection == "scissors" && playerSelection == "rock") {
-            alert("You Win! Rock beats Scissors");
-        }
-
-        if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "paper" && playerSelection == "rock") ||
-        (computerSelection == "scissors" && playerSelection == "paper"))
-        ++computerScore;
-        
-
-        else if(
-        (computerSelection == "rock" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "rock")) 
-    ++playerScore;
-     
-    
-    if (
-        playerScore > computerScore) {
-        alert("Player Wins!");
-       }
-    else if (
-        computerScore > playerScore) {
-        alert("Computer Wins!");
-        }
-    else {
-        alert("Tie game!");
-        }
-       
-       
-
-
-    console.log(playerSelection, computerSelection);
-    console.log(playerScore, computerScore);
+    else if (playerScore ===5) {
+        finalResult.textContent="You win!";
+        buttons.forEach((button) => {
+        button.removeEventListener("click", getPlayerChoice);
+    });
     }
-console.log(playerSelection, computerSelection);
+}
